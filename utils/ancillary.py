7ac155d6fef4c6f54f35816ed2ca083f43ed5d1e
Leonardo <lcoscarella@gmail.com>
2025-10-15 11:56:46 -0300

    Commit con consignas de pre entrega V4

--- utils/ancillary.py
+++ utils/ancillary.py
@@ -1,3 +1,4 @@
+from requests import options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,9 +12,27 @@ sauceDemoCartUrl = 'https://www.saucedemo.com/cart.html'
 
 def getDriver():
 
-    #instalacion de driver
+    # Inicialización de driver
+
+    # Las siguientes options se utilizan para que no aparezcan ventanas emergentes (cambio de contraseña en Chrome)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--disable-features=PasswordCheck")
+    options.add_argument("--disable-features=AutofillServerCommunication")
+
+    options.add_argument("--incognito")
+    options.add_argument("--no-first-run")
+    options.add_argument("--no-default-browser-check")
+
+
+    options.add_experimental_option("prefs", {
+    "credentials_enable_service": False,
+    "profile.password_manager_enabled": False
+    })
+
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service,options=options)
 
     time.sleep(5)
 
